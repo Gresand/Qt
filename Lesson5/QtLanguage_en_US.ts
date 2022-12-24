92187e7b74d4d26ce52081ce346da33f64a1bd30
Андрей Грес <andreygress@gmail.com>
2022-12-24 23:09:10 +0300

    Lesson 5

---
+++ Lesson5/QtLanguage_en_US.ts
@@ -0,0 +1,142 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="form.ui" line="14"/>
+        <source>Справка</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="20"/>
+        <source>TextLabel</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Keyboard</name>
+    <message>
+        <location filename="keyboard.ui" line="38"/>
+        <source>Настройки ракладки клавиатуры</source>
+        <translation>Keyboard layout settings</translation>
+    </message>
+    <message>
+        <location filename="keyboard.ui" line="55"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Открыть&lt;/p&gt;&lt;p&gt;Сохранить&lt;/p&gt;&lt;p&gt;Справка&lt;/p&gt;&lt;p&gt;Выход&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Open&lt;/p&gt;&lt;p&gt;Save&lt;/p&gt;&lt;p&gt;Help&lt;/p&gt;&lt;p&gt;Exit&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="keyboard.ui" line="100"/>
+        <source>По умолчанию</source>
+        <translation>Default</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Блокнот</source>
+        <translation>Notepadjust++</translation>
+    </message>
+    <message>
+        <source>Обзор</source>
+        <translation type="vanished">Review</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="54"/>
+        <source>Файл</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="61"/>
+        <source>Язык</source>
+        <translation>Language</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="91"/>
+        <source>Сохранить</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="96"/>
+        <source>Открыть</source>
+        <translation>Open</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="101"/>
+        <source>Выход</source>
+        <translation>Exit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="106"/>
+        <source>Справка</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="135"/>
+        <source>Открыть в режиме чтения</source>
+        <translation>Open in read mode</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="146"/>
+        <source>Светлая</source>
+        <translation>Light</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="154"/>
+        <source>Тёмная</source>
+        <translation>Dark</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="68"/>
+        <source>Выбрать тему</source>
+        <translation>Theme</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="117"/>
+        <source>Русский</source>
+        <translation>Russian</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="125"/>
+        <source>Английский</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="130"/>
+        <source>Настройки раскладки клавиатуры</source>
+        <translation>Display layout settings</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="73"/>
+        <location filename="mainwindow.cpp" line="93"/>
+        <source>Открыть файл</source>
+        <translation>Open file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="74"/>
+        <location filename="mainwindow.cpp" line="94"/>
+        <location filename="mainwindow.cpp" line="114"/>
+        <source>Текстовые файлы (*.txt);;Файлы C++ (*.cpp *.h)</source>
+        <translation>text file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="80"/>
+        <location filename="mainwindow.cpp" line="100"/>
+        <source>Ошибка</source>
+        <translation>error</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="80"/>
+        <location filename="mainwindow.cpp" line="100"/>
+        <source>Не могу открыть файл</source>
+        <translation>can&apos;t open the file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="113"/>
+        <source>Сохранить файл</source>
+        <translation>save file</translation>
+    </message>
+</context>
+</TS>
